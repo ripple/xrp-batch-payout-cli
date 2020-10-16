c913355fd618d612cbede174a9c01108017d0371
Dino Rodriguez <dinoorodriguez@gmail.com>
2020-10-16 12:07:39 -0400

    update help with prompt overrides object

--- bin/index.js
+++ bin/index.js
@@ -5,11 +5,28 @@ const { payout } = require('../build/src/index')
 const { program } = require('commander')
 const package = require('../package.json')
 
+// Example prompt override object for help output
+const exampleOverrides = {
+  inputCsv: "./input.csv",
+  outputCsv: "./output.csv",
+  network: "mainnet",
+  grpcUrl: "https://envoy.main.xrp.xpring.io",
+  maxFee: 0.01,
+  usdToXrpRate: 0.25,
+  secret: "shBfYr5iEzQWJkCraTESe2FeiPo4e",
+  confirmed: true
+}
+
 // Basic CLI usage for node script + accept user input
 program
   .name(package.name)
   .version(package.version)
   .option('-f, --file <path>', 'JSON file that contains prompt overrides')
+  .on('--help', () => {
+    console.log('')
+    console.log('Example prompt overrides object:')
+    console.log(JSON.stringify(exampleOverrides, null, 2))
+  })
   .parse(process.argv)
 
 // Get overrides if the file path is set

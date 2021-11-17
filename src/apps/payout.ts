be014a6152a1743d5395e1dcfcd10f567e41e298
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-11-17 15:21:01 -0500

    use `xrpl.js` functions

--- src/apps/payout.ts
+++ src/apps/payout.ts
@@ -4,9 +4,10 @@ function long, but not particularly complex. */
 // XRP payout script
 import fs from 'fs'
 
+import {Wallet, Client} from 'xrpl'
 import { ZodError } from 'zod'
 
-import { questions, retryLimit } from '../lib/config'
+import { questions } from '../lib/config'
 import { parseFromCsvToArray, parseFromPromptToObject } from '../lib/io'
 import log, { green, black, red } from '../lib/log'
 import {
@@ -16,11 +17,7 @@ import {
   SenderInput,
   txOutputSchema,
 } from '../lib/schema'
-import {
-  connectToLedger,
-  generateWallet,
-  reliableBatchPayment,
-} from '../lib/xrp'
+import reliableBatchPayment from '../lib/xrp'
 
 /**
  * Run the XRP payout script.
@@ -57,27 +54,27 @@ export default async function payout(override?: unknown): Promise<void> {
     // Generate XRP wallet from secret
     log.info('')
     log.info(`Generating ${senderInput.network} wallet from secret..`)
-    const [wallet, classicAddress] = generateWallet(
-      senderInput.secret,
-      senderInput.network,
-    )
+
+    const wallet = Wallet.fromSecret(senderInput.secret)
     log.info(green('Generated wallet from secret.'))
-    log.info(black(`  -> Sender XRPL X-Address: ${wallet.getAddress()}`))
-    log.info(black(`  -> Sender XRPL Classic address: ${classicAddress}`))
+    log.info(black(`  -> Sender XRPL X-Address: ${wallet.getXAddress()}`))
+    log.info(black(`  -> Sender XRPL Classic address: ${wallet.classicAddress}`))
 
     // Connect to XRPL
     log.info('')
     log.info(`Connecting to XRPL ${senderInput.network}..`)
-    const [xrpNetworkClient, balance] = await connectToLedger(
-      senderInput.grpcUrl,
-      senderInput.network,
-      classicAddress,
-    )
+    
     log.info(green(`Connected to XRPL ${senderInput.network}.`))
     log.info(
       black(`  -> RippleD node web gRPC endpoint: ${senderInput.grpcUrl}`),
     )
-    log.info(black(`  -> ${classicAddress} balance: ${balance} XRP`))
+
+    const client = new Client(senderInput.grpcUrl)
+    await client.connect()
+    log.info(green('Connected'))
+    
+    const balance = await client.getXrpBalance(wallet.classicAddress)
+    log.info(black(`  -> ${wallet.classicAddress} balance: ${balance} XRP`))
 
     // Reliably send XRP to accounts specified in transaction inputs
     const txOutputWriteStream = fs.createWriteStream(senderInput.outputCsv)
@@ -86,9 +83,8 @@ export default async function payout(override?: unknown): Promise<void> {
       txOutputWriteStream,
       txOutputSchema,
       wallet,
-      xrpNetworkClient,
+      client,
       senderInput.usdToXrpRate,
-      retryLimit,
     )
 
     log.info('')
@@ -97,6 +93,8 @@ export default async function payout(override?: unknown): Promise<void> {
         `Batch payout complete succeeded. Reliably sent ${txInputs.length} XRP payments.`,
       ),
     )
+    await client.disconnect();
+
   } catch (err) {
     if (err instanceof ZodError) {
       log.error(err.errors)
